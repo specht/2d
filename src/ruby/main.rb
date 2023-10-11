ab256b0a9e1e79c5617b49a5ebd34efc809b1c1e
Michael Specht <micha.specht@gmail.com>
2023-10-11 12:11:00 +0200

    yay

--- src/ruby/main.rb
+++ src/ruby/main.rb
@@ -17,7 +17,7 @@ $VERBOSE = warn_level
 DASHBOARD_SERVICE = ENV["DASHBOARD_SERVICE"]
 
 PLAYTESTING_CODES = %w(fusx9df 3vkd279 2ia3zeg oiuqhfs q0gqb26 iprehid 4vivnp1 538r5go ru9kt30
-                       5ykixy4 5n8d2u3 dralsk7 b9j7tol qgzpz5l 5nminku mm4gkxu i9cxpyb j642u8i)
+                       5ykixy4 5n8d2u3 dralsk7 b9j7tol qgzpz5l 5nminku mm4gkxu i9cxpyb j642u8i seg3im9)
 
 Neo4jBolt.bolt_host = "neo4j"
 Neo4jBolt.bolt_port = 7687
