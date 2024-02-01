8aeff294359ca29c08c2daabf0c910eeee2d5108
Michael Specht <micha.specht@gmail.com>
2024-02-01 09:50:50 +0100

    yay

--- src/ruby/main.rb
+++ src/ruby/main.rb
@@ -18,7 +18,7 @@ DASHBOARD_SERVICE = ENV["DASHBOARD_SERVICE"]
 
 PLAYTESTING_CODES = %w(e2bnn1m 8598gp7 2lmg98v zf572rq ohbxw98 fd8mqzg l9jkp7u o1umqtz
                        9puw9cl hkgixso cj4qolc p2zz17d dbkrr1s 8pj0l9x 2p6xsag s2i2rlq
-                       lgcqbqy 9i3rtj4 kzrrw45)
+                       lgcqbqy 9i3rtj4 kzrrw45 7mbbnfv)
 
 Neo4jBolt.bolt_host = "neo4j"
 Neo4jBolt.bolt_port = 7687
