be221662741f1087d23e8f87e6f220c1405de023
Michael Specht <specht@gymnasiumsteglitz.de>
2024-10-09 08:58:19 +0200

    yay

--- src/ruby/main.rb
+++ src/ruby/main.rb
@@ -12,7 +12,7 @@ require 'vips'
 DASHBOARD_SERVICE = ENV["DASHBOARD_SERVICE"]
 DEVELOPMENT = ENV['DEVELOPMENT'] == '1'
 
-PLAYTESTING_CODES = %w(qpg4q6p 36axjv4 r12n5ig lwf70x0 dxms3ar fda7jvo mgfj97n k71l96k)
+PLAYTESTING_CODES = %w(qpg4q6p 36axjv4 r12n5ig iwf70x0 dxms3ar fda7jvo mgfj97n k71l9bk 33t7vhf)
 
 Neo4jBolt.bolt_host = "neo4j"
 Neo4jBolt.bolt_port = 7687
